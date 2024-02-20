1a1d179ac65e6b14cde1925e8d3419e104da729e
Côme Laguarigue <comelaguarigue@gmail.com>
2024-02-20 14:55:16 +0000

    add int sales margin

---
+++ models/intermediate/int_sales_margin.sql
@@ -0,0 +1,18 @@
+with 
+
+source as (
+
+    select * from {{ ref('stg_raw__product')}}
+    join {{ ref('stg_raw__sales')}} 
+    using (products_id)
+
+)
+
+select
+    *,
+    round(revenue - CAST(purchase_price as FLOAT64),2) as margin,
+    round(CAST(purchase_price as FLOAT64) * quantity,2) as purchase_cost
+
+    from source
+
+
