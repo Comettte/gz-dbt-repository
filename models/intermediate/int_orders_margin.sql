8cbd7798c203afceed072bb3bf70ecf44ef2cb9a
Côme Laguarigue <comelaguarigue@gmail.com>
2024-02-20 15:15:56 +0000

    add orders margin

--- models/intermediate/int_orders_margin.sql
+++ models/intermediate/int_orders_margin.sql
@@ -1,11 +1,13 @@
 select 
-    orders_id,
-    date_date,
-    revenue,
-    quantity,
-    purchase_cost,
-    margin
+orders_id,
+date_date,
+round(sum(revenue),2) as revenue,
+round(sum(purchase_cost),2) as purchase_cost,
+sum(quantity) as quantity,
+round(sum(margin),2) as margin
 
-    from {{ ref('int_sales_margin') }}
+from {{ ref('int_sales_margin') }}
+    
+group by orders_id, date_date
 
 
